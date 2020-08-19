5e8bc5d1c866978e273a54e52e530a3a0d5cad01
hongrammer <shinerbot@naver.com>
2020-08-19 15:49:10 +0900

    PS

--- source/신촌_E.cpp
+++ source/신촌_E.cpp
@@ -1,3 +1,9 @@
+//
+//  신촌_E.cpp
+//
+//
+//  Created by jm on 2020/08/15.
+//
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,6 +13,26 @@ typedef long long ll;
 string str;
 ll num_list[MAX];
 char char_list[MAX];
+int left_posNum, right_posNum, left_posChar, right_posChar;
+
+ll cal(ll a, ll b, char c){
+    if(c == '+') return a + b;
+    if(c == '-') return a - b;
+    if(c == '*') return a * b;
+    else return a / b;
+}
+
+void left(ll a){
+    num_list[left_posNum+1] = a;
+    left_posNum++;
+    left_posChar++;
+}
+
+void right(ll a){
+    num_list[right_posNum-1] = a;
+    right_posNum--;
+    right_posChar--;
+}
 
 int main(){
     cin >> str;
@@ -29,119 +55,28 @@ int main(){
             pos_num++;
         }
     }
-    if(minus_flag){
-        num_list[pos_num] = -atoll(temp_num.c_str());
-    }else{
-        num_list[pos_num] = atoll(temp_num.c_str());
-    }
+    if(minus_flag) num_list[pos_num] = -atoll(temp_num.c_str());
+    else num_list[pos_num] = atoll(temp_num.c_str());
     pos_num++;
     
     int temp_posChar = pos_char;
-    int left_posNum=0, right_posNum = pos_num - 1, left_posChar = 0, right_posChar = pos_char - 1;
+    left_posNum = 0; right_posNum = pos_num - 1; left_posChar = 0; right_posChar = pos_char - 1;
     while(temp_posChar--){
-        ll compare_left=0, compare_right=0;
+        ll compare_left = cal(num_list[left_posNum], num_list[left_posNum+1], char_list[left_posChar]);
+        ll compare_right = cal(num_list[right_posNum-1], num_list[right_posNum], char_list[right_posChar]);
         
-        if( ((char_list[left_posChar] == '*') || (char_list[left_posChar] == '/' )) && ((char_list[right_posChar] == '*') || (char_list[right_posChar] == '/' )) )
-        { // 양쪽다 곱셈 혹은 나눗셈
-            if(char_list[left_posChar] == '*'){ // 왼쪽이 곱셈인경우
-                compare_left = num_list[left_posNum] * num_list[left_posNum+1];
-                if(char_list[right_posChar] == '/'){ // 왼쪽이 곱셈 오른쪽이 나눗셈
-                    compare_right = num_list[right_posNum-1] / num_list[right_posNum];
-                }else{ // 왼쪽 곱셈 오른쪽 곱셈
-                    compare_right = num_list[right_posNum] * num_list[right_posNum-1];
-                }
-
-                if(compare_left >= compare_right){ // 왼쪽 연산부터
-                    num_list[left_posNum+1] = compare_left;
-                    left_posNum++;
-                    left_posChar++;
-                }else{
-                    num_list[right_posNum-1] = compare_right;
-                    right_posNum--;
-                    right_posChar--;
-                }
-            }else{
-                compare_left = num_list[left_posNum] / num_list[left_posNum+1];
-                if(char_list[right_posChar] == '/'){ // 왼쪽이 나눗셈 오른쪽이 나눗셈
-                    compare_right = num_list[right_posNum-1] / num_list[right_posNum];
-                }else{ // 왼쪽 곱셈 오른쪽 곱셈
-                    compare_right = num_list[right_posNum] * num_list[right_posNum-1];
-                }
-
-                if(compare_left >= compare_right){ // 왼쪽 연산부터
-                    num_list[left_posNum+1] = compare_left;
-                    left_posNum++;
-                    left_posChar++;
-                }else{
-                    num_list[right_posNum-1] = compare_right;
-                    right_posNum--;
-                    right_posChar--;
-                }
-            }
+        if( ((char_list[left_posChar] == '*') || (char_list[left_posChar] == '/' )) && ((char_list[right_posChar] == '*') || (char_list[right_posChar] == '/' )) ){
+                if(compare_left >= compare_right) left(compare_left);
+                else right(compare_right);
         }
-        
-        else if( (char_list[left_posChar] == '*') || (char_list[left_posChar] == '/' )){ //왼쪽이 곱셈나눗셈 오른쪽은 더하기빼기
-            if(char_list[left_posChar] == '*'){ // 왼쪽이 곱셈인경우
-                compare_left = num_list[left_posNum] * num_list[left_posNum+1];
-            }else{
-                compare_left = num_list[left_posNum] / num_list[left_posNum+1];
-            }
-            
-            num_list[left_posNum+1] = compare_left;
-            left_posNum++;
-            left_posChar++;
-        }
-        
-        else if( (char_list[right_posChar] == '*') || (char_list[right_posChar] == '/' ) ){ // 오른쪽이 곱셈 나눗셈 왼쪽은 더하기 빼기
-            if(char_list[right_posChar] == '*'){ // 오른쪽이 곱셈인경우
-                compare_right = num_list[right_posNum-1] * num_list[right_posNum];
-            }else{
-                compare_right = num_list[right_posNum-1] / num_list[right_posNum];
-            }
-            
-            num_list[right_posNum-1] = compare_right;
-            right_posNum--;
-            right_posChar--;
-        }
-        
-        else{ // 왼쪽 오른쪽 둘다 더하기 빼기
-            if(char_list[left_posChar] == '+'){ // 왼쪽이 더하기인경우
-                compare_left = num_list[left_posNum] + num_list[left_posNum+1];
-                if(char_list[right_posChar] == '-'){ // 왼쪽이 더하기 오른쪽이 빼기
-                    compare_right = num_list[right_posNum-1] - num_list[right_posNum];
-                }else{ //
-                    compare_right = num_list[right_posNum] + num_list[right_posNum-1];
-                }
-
-                if(compare_left >= compare_right){ // 왼쪽 연산부터
-                    num_list[left_posNum+1] = compare_left;
-                    left_posNum++;
-                    left_posChar++;
-                }else{
-                    num_list[right_posNum-1] = compare_right;
-                    right_posNum--;
-                    right_posChar--;
-                }
-            }else{ // 왼쪽이 빼기
-                compare_left = num_list[left_posNum] - num_list[left_posNum+1];
-                if(char_list[right_posChar] == '-'){
-                    compare_right = num_list[right_posNum-1] - num_list[right_posNum];
-                }else{
-                    compare_right = num_list[right_posNum] + num_list[right_posNum-1];
-                }
-                
-                if(compare_left >= compare_right){ // 왼쪽 연산부터
-                    num_list[left_posNum+1] = compare_left;
-                    left_posNum++;
-                    left_posChar++;
-                }else{
-                    num_list[right_posNum-1] = compare_right;
-                    right_posNum--;
-                    right_posChar--;
-                }
-            }
+        else if( (char_list[left_posChar] == '*') || (char_list[left_posChar] == '/' )) left(compare_left);
+        else if( (char_list[right_posChar] == '*') || (char_list[right_posChar] == '/' ) ) right(compare_right);
+        else{
+            if(compare_left >= compare_right) left(compare_left);
+            else right(compare_right);
         }
     }
+    
     cout << num_list[left_posNum] << endl;
     
     return 0;
