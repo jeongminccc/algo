d9cfe4ecbc69836028caf330944b9889c00f9f79
hongrammer <shinerbot@naver.com>
2020-08-20 18:46:14 +0900

    PS

---
+++ Tech/1e9.cpp
@@ -0,0 +1,25 @@
+//
+//  1e9.cpp
+//  algo
+//
+//  Created by jm on 2020/08/19.
+//  Copyright © 2020 jm. All rights reserved.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double MOD = 1e9 + 10000;
+    cout << MOD <<  endl;
+    
+    MOD = 10e9;
+    cout << MOD << endl;
+    
+    MOD = -1e9;
+    cout << MOD << endl;
+    
+    MOD = 1e-9;
+    cout << MOD << endl;
+    return 0;
+}
