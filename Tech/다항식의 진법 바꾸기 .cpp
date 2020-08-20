3ddb3c30164769bec525cf7b85a33be95eecabdf
hongrammer <shinerbot@naver.com>
2020-08-20 19:28:16 +0900

    PS

---
+++ Tech/다항식의 진법 바꾸기 .cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void go(int ten, int b) {
+
+	if (ten == 0)
+		return;
+	go(ten / b, b);
+	cout << (ten % b) << " ";
+	return;
+}
+
+int main()
+{
+	int a, b, m;
+	long long num[25], ten = 0;
+
+	cin >> a >> b;
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		cin >> num[i];
+		long long ans = 1;
+		for (int j = 0; j < m - i - 1; j++) {
+			ans *= a;
+		}
+		ans = ans * num[i];
+		ten += ans;
+	}
+
+	go(ten,b);
+
+	return 0;
+}
---
+++ source/이분탐색/이분탐색 - 나무자르기.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+long long list[10001];
+long long least =0, great = 0;
+long long mid, max_num=0;
+int main()
+{
+	int n, k;
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> list[i];
+		if (great <= list[i])
+			great = list[i];
+	}
+	
+	while (least<=great) {
+		mid = (least + great) / 2;
+		int temp = 0;
+
+		for (int i = 0; i < n; i++) {
+			if(list[i] >= mid)
+			temp += list[i] - mid;
+		}
+		if (temp >= k) {
+			if (max_num <= mid)
+				max_num = mid;
+			least = mid + 1;
+		}
+		else {
+			great = mid - 1;
+		}
+	}
+
+	cout << max_num;
+	
+	return 0;
+}
