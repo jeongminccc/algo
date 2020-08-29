e445fd56753e91590a23b53bcc8e733d98ebae9f
hongrammer <shinerbot@naver.com>
2020-08-29 18:50:23 +0900

    PS

--- source/DP/2662_기업투자.cpp
+++ source/DP/2662_기업투자.cpp
@@ -10,38 +10,46 @@
 using namespace std;
 
 int N, M ;
-pair<int, int> inv[301]; // 각 투자 금액 별 최대 이익과, 그 이익을 받을수 있는 기업의 index값이 들어감
-int dp[301][22]; // 답을 구하는 dp배열, 첫번째칸에는 투자 금액별 최대 이익이, 두번째 ~ M+1 칸에는 기업별 투자한 금액이 저장
+int inv[301][21]; // 각 투자 금액 별 이익과 기업의 번호
+int dp[301][21]; // 답을 구하는 dp배열, 첫번째칸에는 투자 금액별 최대 이익이, 두번째 ~ M+1 칸에는 기업별 투자한 금액이 저장
 
+int foo(int sumOfInvest, int company){
+    if(company > M) return 0;
+    
+    int& ret = dp[sumOfInvest][company];
+    if(ret != -1) return ret;
+    
+    ret = 0;
+    for(int invest=0; invest <= sumOfInvest; invest++){
+        // 각 투자금액
+        int temp = inv[invest][company] + foo(sumOfInvest - invest, company+1);
+        if(ret < temp){ // 이번 company에 invest만큼 투자했을때의 이익이 가장 높다면
+            ret = temp;
+            dp[sumOfInvest][company] = invest;
+        }
+    }
+    return ret;
+}
 int main(){
     cin >> N >> M;
     
-    int invest, A;
+    int invest;
     for(int i=1; i<=N; i++){
         cin >> invest;
-        int max_inv = 0, max_index=0;
-        for(int j=0; j<M; j++){
-            cin >> A;
-            if(max_inv <= A){
-                max_inv = A;
-                max_index = j;
-            }
-        }
-        inv[i].first = max_inv;
-        inv[i].second = max_index;
+        for(int j=1; j<=M; j++) cin >> inv[invest][j];
     }
+    memset(dp, -1, sizeof(dp));
     
-    for(int i=1; i<=N; i++){ // 투자 금액별 이익이 들어있는 배열을 사용
-        for(int j=N; j>=i; --j){ // 맨 마지막 부터(한번씩만 사용 가능 하므로) 요소들을 넣어가며 처리
-            if(dp[j][0] <= dp[j-i][0] + inv[i].first){ // inv[i]를 넣었을때 최대가 되는지
-                
-                dp[j][0] = dp[j-i][0] + inv[i].first; // 최대가 되면 값을 바꾸고
-                for(int k=1; k<22; k++) dp[j][k] = dp[j-i][k]; // 기업별 투자금액 index도 복사후
-                dp[j][inv[i].second+1] += i; // 값을 추가
-            }
-        }
+    
+    cout << foo(N, 1) << endl; // N만큼 투자했을때 최대 이익 출력
+    
+    int totalInvest = N;
+
+    for (int i = 1; i <= M; i++){
+         cout << dp[totalInvest][i] << " ";
+         totalInvest -= dp[totalInvest][i];
     }
-    cout << dp[N][0] << endl; // N만큼 투자했을때 최대 이익 출력
-    for(int i=1; i<=M; i++) cout << dp[N][i] << " "; // 각 기업별 투자 금액 출력
+    cout << endl;
+
     return 0;
 }
