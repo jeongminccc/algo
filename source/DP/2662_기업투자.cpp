4915b3f5b8bb4ea1b86f62f1d4eb6dcb85cb2fe3
hongrammer <shinerbot@naver.com>
2020-08-30 16:00:48 +0900

    PS

--- source/DP/2662_기업투자.cpp
+++ source/DP/2662_기업투자.cpp
@@ -12,6 +12,7 @@ using namespace std;
 int N, M ;
 int inv[301][21]; // 각 투자 금액 별 이익과 기업의 번호
 int dp[301][21]; // 답을 구하는 dp배열, 첫번째칸에는 투자 금액별 최대 이익이, 두번째 ~ M+1 칸에는 기업별 투자한 금액이 저장
+int invested[301][21]; // 얼마나 투자를 했는지 저장
 
 int foo(int sumOfInvest, int company){
     if(company > M) return 0;
@@ -25,7 +26,7 @@ int foo(int sumOfInvest, int company){
         int temp = inv[invest][company] + foo(sumOfInvest - invest, company+1);
         if(ret < temp){ // 이번 company에 invest만큼 투자했을때의 이익이 가장 높다면
             ret = temp;
-            dp[sumOfInvest][company] = invest;
+            invested[sumOfInvest][company] = invest;
         }
     }
     return ret;
@@ -40,16 +41,16 @@ int main(){
     }
     memset(dp, -1, sizeof(dp));
     
-    
     cout << foo(N, 1) << endl; // N만큼 투자했을때 최대 이익 출력
     
     int totalInvest = N;
 
     for (int i = 1; i <= M; i++){
-         cout << dp[totalInvest][i] << " ";
-         totalInvest -= dp[totalInvest][i];
+         cout << invested[totalInvest][i] << " ";
+         totalInvest -= invested[totalInvest][i];
     }
     cout << endl;
 
     return 0;
 }
+
