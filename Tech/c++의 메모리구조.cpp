c8545d8e4a066f4785005aca0d294e137890aa51
hongrammer <shinerbot@naver.com>
2020-09-02 19:57:24 +0900

    PS

---
+++ Tech/c++의 메모리구조.cpp
@@ -0,0 +1,49 @@
+//
+//  c++의 메모리구조.cpp
+//  algo
+//
+//  Created by jm on 2020/09/02.
+//  Copyright © 2020 jm. All rights reserved.
+//
+
+//          코드 영역          //
+// 데이터 영역(전역 변수, static) //
+//          힙 영역           //
+//         스택 영역          //
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct POINT{
+    int m_STR, m_INT, m_PNT;
+};
+
+int N, ans=0;
+
+void foo(struct POINT a){
+    cout << "스택 영역에 할당된 foo에서 만들어진 구조체 : " << &a << endl;
+    return;
+}
+
+int main(){
+    static int q;
+    cout << "데이터 영역에 할당된 전역 변수 : " << &N << endl;
+    cout << "데이터 영역에 할당된 static 변수 : " << &q << endl;
+    
+    int *arr = new int[10];
+    cout << "힙 영역에 할당된 배열 : " << arr << endl;
+    delete[] arr;
+    
+    POINT a;
+    cout << "스택 영역에 1번째로 할당된 구조체 : " << &a << endl;
+    foo(a);
+    
+    POINT b;
+    cout << "스택 영역에 2번째로 할당된 구조체 : " << &b << endl;
+    int c;
+    cout << "스택 영역에 3번째로 할당된 변수 : " << &c << endl;
+    
+    return 0;
+}
+
---
+++ Tech/참조 매개변수 사용법.cpp
@@ -0,0 +1,67 @@
+//
+//  참조 매개변수 사용법.cpp
+//  algo
+//
+//  Created by jm on 2020/09/02.
+//  Copyright © 2020 jm. All rights reserved.
+//
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 구조체명은 클래스명과 구분하기 위해 모두 대문자로 기입(클래스는 처음글자만 대문자)
+struct STRUCT_A{
+    int m_a; // 멤버변수들은 로컬변수들괴 구분을 위해 m을 붙여줌
+};
+
+struct STRUCT_B{
+    int m_b;
+}nameOfStruct_b; // 이렇게 바로 선언을 해줄 수도 있다.
+
+struct{
+    int m_c;
+}nameOfStruct_c; // 이렇게 바로 선언을 해줄 수도 있다. 근데 매개변수로 전달하거나 할때 불가능함.
+
+
+void foo_Value(struct STRUCT_A a){
+    a.m_a = 2;
+    return;
+}
+
+void foo_Reference(struct STRUCT_B& a){ // 참조로 구조체를 받음.
+    a.m_b = 2; // 평소 구조체 쓰듯이 쓰면 됨.
+    return;
+}
+
+void foo_Reference2(struct STRUCT_B* a){ // 구조체의 주소를 받아 포인터로 값을 변경.
+    (*a).m_b = 3; // 포인터로 받았기에
+    a->m_b = 3;   // 두가지 사용법이 존재
+    return;
+}
+
+int main(){
+    
+    // 구조체 선언시 둘다 가능.
+    STRUCT_A nameOfStruct_a;
+    struct STRUCT_A nameOfStruct_a2;
+    
+    nameOfStruct_a.m_a = 1;
+    nameOfStruct_b.m_b = 1;
+    nameOfStruct_c.m_c = 1;
+    
+    cout << "값에의한 전달이 이루어 지기 전 : " << nameOfStruct_a.m_a << endl;
+    foo_Value(nameOfStruct_a);
+    cout << "값에의한 전달이 이루어 진 후 : " << nameOfStruct_a.m_a << endl;
+    
+    cout << "참조에의한 전달이 이루어 지기 전 : " << nameOfStruct_b.m_b << endl;
+    foo_Reference(nameOfStruct_b);
+    cout << "참조에의한 전달이 이루어 진 후 : " << nameOfStruct_b.m_b << endl;
+    
+    // 다른 형태의 참조전달
+    cout << "참조에의한 전달이 이루어 지기 전 : " << nameOfStruct_b.m_b << endl;
+    foo_Reference2(&nameOfStruct_b);
+    cout << "참조에의한 전달이 이루어 진 후 : " << nameOfStruct_b.m_b << endl;
+    
+    return 0;
+}
